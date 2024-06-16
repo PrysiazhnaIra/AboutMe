ca106819e88fb566d1d5575ab44c677b6d26cbf9
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-06-16 14:25:33 +0300

    trials-two

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './js/am';
-import myPhoto from './img/photo.png';
-document.getElementById('my-photo').src = myPhoto;
+// import myPhoto from './img/photo.png';
+// document.getElementById('my-photo').src = myPhoto;
 
 import './js/faq';
