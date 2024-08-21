63ca0786acf7791774ed1d3a3061efbfc86caf68
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-08-21 12:16:11 +0300

    animation

--- src/js/am.js
+++ src/js/am.js
@@ -6,3 +6,23 @@ setInterval(() => {
   butterfly.style.top = `${Math.random() * window.innerHeight}px`;
   butterfly.style.left = `${Math.random() * window.innerWidth}px`;
 }, 2000);
+
+// animation
+window.addEventListener('DOMContentLoaded', () => {
+  const items = document.querySelectorAll('.about-item');
+  items.forEach((item, index) => {
+    setTimeout(() => {
+      item.classList.add('slide-in');
+    }, index * 200); // Staggered animation
+  });
+});
+
+// animation type2
+// window.addEventListener('DOMContentLoaded', () => {
+//   const items = document.querySelectorAll('.about-item');
+//   items.forEach((item, index) => {
+//     setTimeout(() => {
+//       item.classList.add('fade-in');
+//     }, index * 200); // Staggered animation
+//   });
+// });
